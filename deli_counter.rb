86fd3f66dc889b3808f781a4acb59df96b0a5e85
Chris Gullickson <cgullickson5@gmail.com>
2017-11-21 21:03:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def line (katz_deli)
   else
     message = "The line is currently:"
     katz_deli.each_with_index do |name,index|
-      message += "#{index+1}. #{name}"
+      message += "#{index+1}. #{name} "
     end
     puts message
   end
